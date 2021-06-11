98de07b6ffd347c632e1dd1632c9272f0dcb6854
Shef <Sefunmi@vt.edu>
2021-06-11 08:04:35 -0400

    added text flied component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Header from "./component/header"
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+import React from 'react';
+import Task_Text_Field from "./component/task-text-field"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function App() {
   return (
+    //display components
     <div className="App">
-    <Header/>
+      <Header/>
+      <Task_Text_Field/>
     </div>
   );
 }
---
+++ src/component/task-text-field/task-text-field.js
@@ -0,0 +1,10 @@
+import TextField from '@material-ui/core/TextField';
+
+
+export default function Tas_Text_Field(){
+    return (
+        <div>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+    );
+}

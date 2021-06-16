3dc3ff938cc65b15ba6a885ca98aaa0d9ac431c8
B-o-s-s-e-s <63765098+B-o-s-s-e-s@users.noreply.github.com>
2021-06-16 18:35:39 -0400

    Update header.js

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,4 +1,28 @@
-//TODO: add style to header display-Pika
-export default function Header(){
-    return <div>2021 IBM Accelerate Software Track Lab1 : To-Do List</div>
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
+
+export default function Types() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+
+      <Typography variant="h4" gutterBottom>
+      2021 IBM Accelerate Software Group #39: To-Do List
+      </Typography> 
+      <Typography variant="body1" gutterBottom>
+      We created and included both the front and end of a to-do list web application by using React 
+      , Material UI components, and Javascript. Allow the users to organize tasks.
+      </Typography>
+      
+    </div>
+  );
 }
